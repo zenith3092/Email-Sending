73feec8093396af7f87d447d34e234a824b17087
zenith3092 <xianglin3092@gmail.com>
2022-06-30 12:07:36 +0800

    upload first version

---
+++ _Email.py
@@ -0,0 +1,42 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+from pathlib import Path
+
+def send_mail(sender_address, sender_passwords, receiver_address, subject, content, image=None):
+    """
+    You should use google app passwords as your sender_passwords.
+    Please refer to the webside below,and mind whether you have turned on the 2-Step Verification or not.
+    https://support.google.com/accounts/answer/185833?hl=en
+
+     Input:
+      sender_address: str
+      sender_passwords: str
+      receiver_address: str
+      subject: str
+      content: str
+      **image: list of str
+     Output:
+      If no error exises ->  return True
+      If errors exist  ->  return False
+    """
+    message = MIMEMultipart()
+    message['From'] = sender_address
+    message['To'] = receiver_address
+    message['Subject'] = subject   
+    message.attach(MIMEText(content, 'plain'))    
+    if image != None:
+        for i in range(len(image)):
+            message.attach(MIMEImage(Path(image[i]).read_bytes()))
+    session = smtplib.SMTP('smtp.gmail.com', 587)
+    try:
+        session.starttls()
+        session.login(sender_address, sender_passwords)
+        text = message.as_string()
+        session.sendmail(sender_address, receiver_address, text)
+        session.quit()
+        return True
+    except Exception as e:
+        print("Error message: ", e)
+        return False
---
+++ email test.py
@@ -0,0 +1,14 @@
+import _Email
+
+sender_address = 'xxxxxxxx@gmail.com'
+sender_passwords = 'xxxxxxxxxxxxxxxx'
+receiver_address = 'xxxxxxxx@gmail.com'
+subject= "Test subject"
+content = '''Hello!
+This is a test.
+I'm attaching the two images in this email.
+'''
+image = ["star.png", "photo.jpg"]
+
+flag = _Email.send_mail(sender_address, sender_passwords, receiver_address, subject, content, image)
+print(flag)
